6e71d89a62459935868357a47daca4a053e79397
vinh0407 <118229444+vinh0407@users.noreply.github.com>
2025-03-20 17:02:32 +0700

    Add files via upload

---
+++ QuizGameSever/Models/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+public class Question
+{
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public List<string> Options { get; set; }
+    public int CorrectAnswer { get; set; }
+
+    public Question(int id, string content, List<string> options, int correctAnswer)
+    {
+        Id = id;
+        Content = content;
+        Options = options;
+        CorrectAnswer = correctAnswer;
+    }
+}
